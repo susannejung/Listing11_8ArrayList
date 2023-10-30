6d9c3ef6ff9567ca680aa832c3053f4468e89efd
SUHA <suha@zealand.dk>
2023-10-30 15:41:02 +0100

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,34 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
